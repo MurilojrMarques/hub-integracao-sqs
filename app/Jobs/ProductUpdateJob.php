0a98c454ca772d122d551248f1f160ebfe4277fb
MuriloJrMarques <muriloj2702@gmail.com>
2026-05-11 19:28:22 -0300

    feat: adicionando logs de monitoramento dos jobs

--- app/Jobs/ProductUpdateJob.php
+++ app/Jobs/ProductUpdateJob.php
@@ -7,6 +7,7 @@ use App\Enums\ProductUpdateType;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
 
 class ProductUpdateJob implements ShouldQueue, ShouldBeUnique
 {
@@ -21,11 +22,15 @@ class ProductUpdateJob implements ShouldQueue, ShouldBeUnique
     ) {}
 
     public function handle(): void
-    {
+    {   
+        Log::info("Processando job: {$this->uniqueId()} para o produto {$this->sku}");
+        
+
         $product = Product::where('sku', $this->sku)->first();
 
         if (!$product) {
-            throw new \Exception("O produto não foi encontrado."); 
+            $this->fail(new \Exception("O produto com SKU {$this->sku} não foi encontrado."));
+            return;
         }
 
         match($this->type) {
@@ -37,10 +42,20 @@ class ProductUpdateJob implements ShouldQueue, ShouldBeUnique
         };
 
         $product->save();
+
+        Log::info("Job processado com sucesso: {$this->uniqueId()} para o produto {$this->sku}");
     }
 
     public function uniqueId(): string
     {
         return $this->sku . '-' . $this->type->value . '-' . md5(json_encode($this->data));
+    }
+
+    public function failed(\Throwable $exception): void{
+        Log::error("ALERTA CRÍTICO: Job falhou: {$this->uniqueId()} para o produto {$this->sku}", [
+            'type' => $this->type->value,
+            'payload' => $this->data,
+            'error_message' => $exception->getMessage(),
+        ]);
     }
 }
